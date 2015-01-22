19d686b007943d58f5e93cde8288ce36b4424bb5
Xanathar <marco@mastropaolo.com>
2015-01-22 18:38:10 +0100

    Missing DefaultSlide.lua

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ Product trademarks, logos, names, together with brands and any other trademark f
 simply showed within the present material are the property of their respective rightful owner, 
 trademark holders and Licensors. 
 
+Features:
+
+* Supports standard slides in any easy way
+* Slides can be scripted in Lua
+* HLSL shaders are supported
+* Fade effect between slides
 
 
 
---
+++ src/GlobalGameJam2015Presentation/Scripts/DefaultSlide.lua
@@ -0,0 +1,30 @@
+﻿title = text(_TEXT, _FONT, _FONTSIZE);
+
+distractions = {
+	"Distractions/Bomberman.lua",
+	"Distractions/Zelda.lua",
+	"Distractions/ZeldaRun.lua",
+};
+
+if (_OPTIONS.distractionfree) then
+	distraction = { reset = function() end, draw = function() end };
+	distractionTime = 0;
+else
+	local distractionidx = math.random(1, #distractions);
+	dofile(distractions[distractionidx]);
+	distractionTime = math.random(1, 120);
+end
+
+
+function reset()
+	distraction:reset();
+end
+
+function draw(time, frame)
+	title.draw();
+	
+	if (time >= distractionTime) then
+		distraction:draw(time - distractionTime);
+	end
+	
+end
